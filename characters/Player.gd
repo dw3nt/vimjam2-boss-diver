0083f01f938bca18433da68638f2c3457fe94743
dw3nt <rick@dualwieldent.com>
2021-09-10 22:03:33 -0700

    player walk input

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,3 +1,23 @@
 extends KinematicBody2D
+class_name Player
 
-export(float) var walkSpeed = 100
+export(float) var walkSpeed = 50
+
+var canWalk : bool = false
+var isWalking : bool = false
+
+
+func _input(event) -> void:
+	if canWalk && event.is_action_pressed("walk"):
+		if !isWalking:
+			print('walk')
+			isWalking = true
+		else:
+			print('stop')
+			isWalking = false
+			canWalk = false
+		
+		
+func _physics_process(_delta) -> void:
+	if isWalking:
+		move_and_slide(Vector2.RIGHT * walkSpeed)
