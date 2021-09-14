ab157591afb4c7dc86be404070ef9aacb302db69
dw3nt <rick@dualwieldent.com>
2021-09-14 11:53:40 -0700

    updated pool animations

--- objects/Pool.gd
+++ objects/Pool.gd
@@ -1,17 +1,7 @@
 extends Area2D
 class_name Pool
 
-var moveSpeed : = 6.0
-
 signal pool_entered
-
-onready var sprite = $Sprite
-
-
-func _process(delta : float) -> void:
-	sprite.region_rect.position.x += moveSpeed * delta
-	if abs(sprite.region_rect.position.x) > 12:
-		moveSpeed *= -1
 
 
 func _on_Pool_body_entered(body : Player):
