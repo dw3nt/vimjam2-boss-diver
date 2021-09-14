c5f7415b124c0ac5341c6ef4373349a1f4801737
dw3nt <rick@dualwieldent.com>
2021-09-14 15:25:06 -0700

    new ladder art and adjusted paths

--- characters/ClimbLadder.gd
+++ characters/ClimbLadder.gd
@@ -1,14 +1,13 @@
 extends PlayerState
 
-const CLIMB_SPEED : = 100
+const CLIMB_SPEED : = 80
 
-var pathAnimMap = {
-	0: "walk",
-	1: "climb",
+var pathAnimMap = {	
+	1: "walk",
 	2: "climb",
 	3: "climb",
 	4: "walk",
-	5: "idle",
+	5: "idle"
 }
 
 var path : Path2D
@@ -22,16 +21,20 @@ func enter_state(params : Dictionary = {}) -> void:
 	
 	
 func process(delta : float) -> void:
+	if pathFollow.unit_offset == 0.0:
+		print(global_position)
+	
 	if pathFollow.unit_offset < 1.0:
 		pathFollow.offset += CLIMB_SPEED * delta
 	else:
 		fsm.change_state("WalkBoard")
 		
-	if global_position.x >= path.curve.get_point_position(nextPointIndex).x:
+		
+	if nextPointIndex < path.curve.get_point_count() && global_position.x > path.curve.get_point_position(nextPointIndex).x:
 		nextPointIndex += 1
 		updateClimbAnimation()
 		
 		
 func updateClimbAnimation() -> void:
-	if pathAnimMap.keys().has(nextPointIndex - 1):
-		fsm.anim.play(pathAnimMap[nextPointIndex - 1])
+	if pathAnimMap.keys().has(nextPointIndex):
+		fsm.anim.play(pathAnimMap[nextPointIndex])
--- objects/Pool.gd
+++ objects/Pool.gd
@@ -4,6 +4,12 @@ class_name Pool
 signal pool_entered
 
 
+func _ready() -> void:
+	$Sprite/AnimationPlayer.play("pool_idle", -1, 0.5)
+	$Sprite2/AnimationPlayer.play("pool_idle", -1, 0.5)
+	$Sprite3/AnimationPlayer.play("pool_idle", -1, 0.5)
+
+
 func _on_Pool_body_entered(body : Player):
 	if body:
 		body.enteredPool()
