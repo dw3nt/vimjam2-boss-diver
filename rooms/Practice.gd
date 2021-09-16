d881aadf4b76c80b372174bceb5effbac2fc490d
dw3nt <rick@dualwieldent.com>
2021-09-16 11:54:46 -0700

    practice mode as its own scene

---
+++ rooms/Practice.gd
@@ -0,0 +1,37 @@
+extends "res://rooms/BaseRoom.gd"
+
+export(String, FILE, "*.tscn") var mainMenuScene
+
+onready var player = $Player
+onready var judgesTable = $JudgesTable
+onready var diveResults = $CanvasLayer/DiveResults
+
+
+func _ready() -> void:
+	randomize()
+	emit_signal("room_ready")
+
+
+func _on_Player_entered_pool(isSloppy : bool) -> void:
+	var speed = 1.0
+	if isSloppy:
+		speed = 0.0
+	else:
+		judgesTable.playClap()
+	
+	judgesTable.playHeadBobAtSpeed(speed)
+
+
+func _on_Floor_dive_finished():
+	diveResults.points.append(player.stateWrap.points1)
+	diveResults.points.append(player.stateWrap.points2)
+	diveResults.points.append(player.stateWrap.points3)
+	diveResults.anim.play("fade_in")
+
+
+func _on_DiveResults_retry_button_pressed():
+	emit_signal("room_change_requested", { "scene": filename })
+
+
+func _on_DiveResults_main_menu_button_pressed():
+	emit_signal("room_change_requested", { "scene": mainMenuScene })
